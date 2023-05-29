6e1d7cac3e44dd11376efad7553f03cc5190c61b
Hellcomander <danielarandareyes>
2023-05-29 10:04:57 -0600

    Splash screen

--- Reysol/app/src/main/java/com/example/reysol/MainActivity.java
+++ Reysol/app/src/main/java/com/example/reysol/MainActivity.java
@@ -8,6 +8,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        setTheme(R.style.Theme_Reysol);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
