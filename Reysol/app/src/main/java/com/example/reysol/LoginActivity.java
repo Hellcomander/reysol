f6019b655209458b6382da52bec29c06b0587b13
juanxd01 <80122507+juanxd01@users.noreply.github.com>
2023-05-30 13:11:38 -0600

    login

--- Reysol/app/src/main/java/com/example/reysol/LoginActivity.java
+++ Reysol/app/src/main/java/com/example/reysol/LoginActivity.java
@@ -3,12 +3,35 @@ package com.example.reysol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText txtEmail, txtPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password = txtPassword.getText().toString();
+                String email = txtEmail.getText().toString();
+
+                if(password.equals("") || email.equals("")){
+                    Toast.makeText(getApplicationContext(), "INGRESA TODOS LOS DATOS", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        });
     }
 }
