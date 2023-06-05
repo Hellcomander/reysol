ca03628a062bf4a3b39aa104af1cf562c8a7be24
Hellcomander <danielarandareyes>
2023-06-05 09:12:47 -0600

    Pantalla de registrar usuario

--- Reysol/app/src/main/java/com/example/reysol/LoginActivity.java
+++ Reysol/app/src/main/java/com/example/reysol/LoginActivity.java
@@ -2,10 +2,12 @@ package com.example.reysol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.reysol.Classes.Clientes;
@@ -16,6 +18,8 @@ public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
     Usuarios usuarios[];
     Clientes clientes[];
+
+    TextView btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +33,19 @@ public class LoginActivity extends AppCompatActivity {
         txtPassword = (EditText) findViewById(R.id.txtPassword);
         btnLogin = (Button) findViewById(R.id.btnLogin);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String password = txtPassword.getText().toString();
-                String email = txtEmail.getText().toString();
+        btnRegister = (TextView) findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(v -> {
+            Intent openRegister = new Intent(this, RegisterActivity.class);
+            startActivity(openRegister);
+        });
 
-                if(password.equals("") || email.equals("")){
-                    Toast.makeText(getApplicationContext(), "INGRESA TODOS LOS DATOS", Toast.LENGTH_LONG).show();
-                    return;
-                }
+        btnLogin.setOnClickListener(v -> {
+            String password = txtPassword.getText().toString();
+            String email = txtEmail.getText().toString();
+
+            if(password.equals("") || email.equals("")){
+                Toast.makeText(getApplicationContext(), "INGRESA TODOS LOS DATOS", Toast.LENGTH_LONG).show();
+                return;
             }
         });
     }
